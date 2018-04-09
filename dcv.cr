d9d3268d6bc53e068dabee4f76f01a8901a932ea
omerxx <omerxx@gmail.com>
2018-04-09 17:23:55 +0300

    WIP: working http request

--- dcv.cr
+++ dcv.cr
@@ -3,11 +3,11 @@
 require "http/client"
 
 class VersionTag
-	@key : 				String
-	@application: String
-	@subsystem:		String
-	@name: 				String
-	@timestamp: 	String
+	@key :					String
+	@application : 	String
+	@subsystem :		String
+	@name : 				String
+	@timestamp : 		String
 	
 	getter :key
 	getter :application
@@ -26,15 +26,24 @@ class VersionTag
 
 end
 
+
 def param(name)
 	return ENV["PLUGIN_#{name.upcase}"]
 end
 
-def 
 
-test = VersionTag.new("omer", "222", "something", "somewhere", "awesome")
-puts test.key
-puts param("omer")
+def createTag(vtag)
+	response = HTTP::Client.get("https://api.coralogix.com/api/v1/addTag?key=#{vtag.key}&application=#{vtag.application}&subsystem=#{vtag.subsystem}&name=#{vtag.name}&timestamp=#{vtag.timestamp}")
+	puts response
+end
+
+key = ENV["COR_KEY"]
+vtag = VersionTag.new(key, "staging", "all", "Omer", Time.now.to_s("%Y-%m-%d:%H:%M:%S"))
+#puts Time.now.to_s("%Y-%m-%d:%H:%M:%S")
+#YYYY-MM-DD:HH:mm:ss
+#puts test.key
+#puts param("omer")
+createTag(vtag)
 
 
 
