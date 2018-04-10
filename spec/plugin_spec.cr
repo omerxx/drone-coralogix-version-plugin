791a041d83fe1d36a88589bcb5f1910d82640b4c
omerxx <omerxx@gmail.com>
2018-04-10 12:57:52 +0300

    Fixed env settings with special param timestamp?

--- spec/plugin_spec.cr
+++ spec/plugin_spec.cr
@@ -1,7 +1,5 @@
 require "spec"
-require "../src/dcv"
-
-key = ENV["COR_KEY"]
+require "../src/plugin"
 
 describe "Parameter function is functional: " do
   it "correctly returnes the uppercase PLUGIN_ form" do
@@ -23,7 +21,7 @@ describe "Parameter function is functional: " do
 end
 
 describe "Creating a tag: " do
-  it "raises an error if one of the class fields are missing" do 
+  it "raises an error if one of the class fields that is no timestamp are missing" do 
     expect_raises(PluginException) do
       vtag = VersionTag.new("", "staging", "all", "Omer", Time.now.to_s("%Y-%m-%d:%H:%M:%S"))
       createTag(vtag)
@@ -34,11 +32,9 @@ end
 describe "Running the Plugin: " do
   it "raises and error if one of the parameters are missing" do 
     expect_raises(KeyError) do
-      fields = ["key", "subsystem", "name", "timestamp"]
-      fields.each { |field| ENV[field] = "x" }
+      ["key", "subsystem", "name", "timestamp"].each { |field| ENV[field] = "x" }
       run!
     end
   end
 end
-
 
--- src/run.cr
+++ src/run.cr
@@ -1,3 +1,3 @@
-require "./dcv"
+require "./plugin"
 
 run!
